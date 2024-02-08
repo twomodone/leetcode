7aff4da67d36f7b155dd26237c91f65993b7b51e
twomodone <twomodone@gmail.com>
2024-02-08 22:36:07 +0200

    Add 1480 solution

---
+++ javascript/easy/1480.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    for (let i = 1; i < nums.length; i++){
+        nums[i] = nums[i] + nums[i - 1];
+    } return nums;
+};
